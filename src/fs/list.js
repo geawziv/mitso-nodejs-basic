6b2573c6a07ad45e51bd9c72f656754abab7b5b0
geawziv <vizwaeg@yandex.by>
2023-03-08 22:30:06 +0300

    feat: finish list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,12 @@
+import fs from "fs/promises";
+import url from "url";
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+    let files = await fs.readdir(`${dirname}files`)
+        .catch((err) => {throw new Error("FS operation failed")});
+    console.log(files);
 };
 
 await list();
