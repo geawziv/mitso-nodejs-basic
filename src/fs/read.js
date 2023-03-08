2ef52a9060c01748d68cb5434f81df7e107e5cf6
geawziv <vizwaeg@yandex.by>
2023-03-08 22:38:01 +0300

    feat: finish read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import fs from "fs/promises";
+import url from "url";
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    let data = await fs.readFile(`${dirname}files/fileToRead.txt`, "utf-8")
+        .catch((err) => {
+            throw new Error("FS operation failed")
+        });
+    console.log(data);
 };
 
 await read();
