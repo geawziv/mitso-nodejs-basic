22e170ca81685a80d949702a7bd9d01084ae741f
geawziv <vizwaeg@yandex.by>
2023-03-08 22:14:08 +0300

    feat: finish create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,11 @@
+import fs from "fs/promises";
+import url from "url";
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+    await fs.writeFile(`${dirname}files/fresh.txt`, "I am fresh and young")
+          .catch((err) => {throw new Error("FS operation failed")})
 };
 
 await create();
