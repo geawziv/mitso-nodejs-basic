e9d101e727773cb6e3b7e7afbd43301f4f4fc656
geawziv <vizwaeg@yandex.by>
2023-03-08 22:53:30 +0300

    feat: finish rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from "fs/promises";
+import url from "url";
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
-};
+    let data = await fs.readFile(`${dirname}files/wrongFilename.txt`)
+        .catch((err) => {throw new Error("FS operation failed")});
+
+    await fs.readFile("src/fs/files/properFilename.md")
+        .catch((_err) => {})
+        .then((res) => {
+            if (res) { throw new Error("FS operation failed") }
+        });
+
+    await fs.writeFile(`${dirname}files/properFilename.md`, data)
+        .catch((err) => { throw new Error("FS operation failed") });
+};  
 
 await rename();
