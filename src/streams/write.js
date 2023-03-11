aae85368c864d46024103e0d569291f037ff49ef
geawziv <vizwaeg@yandex.by>
2023-03-11 20:12:00 +0300

    feat: finish write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import {promises as fs, createWriteStream} from "fs";
+import { stdin } from "process";
+import url from "url";
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const streamOut = createWriteStream(`${dirname}files/fileToWrite.txt`, "utf-8");
+
 const write = async () => {
-    // Write your code here 
+    console.log(dirname);
+    stdin.pipe(streamOut);
 };
 
 await write();
