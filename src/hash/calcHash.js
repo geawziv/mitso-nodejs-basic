43f72d543f95061b34c25e381318c5d156e2e6e9
geawziv <vizwaeg@yandex.by>
2023-03-11 18:51:14 +0300

    feat: finish calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import { createHash } from "crypto";
+import fs from "fs/promises";
+import url from "url";
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = await fs.readFile(`${dirname}files/fileToCalculateHashFor.txt`);
+    console.log(createHash('sha256').update(hash).digest("hex"));
 };
 
 await calculateHash();
