aa48f4ef37800138b3cd46944674885a159917b8
geawziv <vizwaeg@yandex.by>
2023-03-11 19:34:06 +0300

    feat: finish read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from "fs"
+import {stdout, stderr} from "process"
+import url from "url"
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const streamIn = fs.createReadStream(`${dirname}files/fileToRead.txt`, "utf-8");
+
 const read = async () => {
-    // Write your code here 
+    let data = "";
+
+    streamIn.on("data", chunk => data += chunk);
+    process.on("exit", code => {
+        if (code === 0) { stdout.write(data); }
+        else { stderr.write("smt broke :("); }
+    })
 };
 
 await read();
