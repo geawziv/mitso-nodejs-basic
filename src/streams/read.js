ce7a06641b019fe81a12366ec0ddb723424b65bf
geawziv <vizwaeg@yandex.by>
2023-03-11 19:58:46 +0300

    feat: finish transform.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,6 +1,6 @@
-import fs from "fs"
-import {stdout, stderr} from "process"
-import url from "url"
+import fs from "fs";
+import {stdout, stderr} from "process";
+import url from "url";
 
 const dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const streamIn = fs.createReadStream(`${dirname}files/fileToRead.txt`, "utf-8");
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,32 @@
+import {stdin, stdout} from "process";
+import { pipeline, Transform } from "stream";
+
+class myTransform extends Transform {
+    constructor(opr) {
+        super(opr);
+    }
+
+    _transform(chunk, encoding, callback) {
+        const data = chunk
+            .toString()
+            .replace("\n", "")
+            .split("")
+            .reverse()
+            .join("");
+
+        this.push(data + "\n");
+        callback(null);
+    }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const transformt = new myTransform();
+    pipeline(
+        stdin,
+        transformt,
+        stdout,
+        (err) => { throw new Error("Stream operation failed"); }
+    )
 };
 
 await transform();
